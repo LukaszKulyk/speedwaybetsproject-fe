afcf7cda3751b516e515948c8b05216122528b0f
lukasz kulyk <lukasz.kulyk@gmail.com>
2021-03-24 12:38:07 +0100

    Adding footer.

---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="footer row justify-content-md-center">
+        <!--<h3>QuickIT Production<br></h3>-->
+        <!--<h4>Contact US</h4>-->
+        <!--<p>Have you found a bug?</p>-->
+        <!--<p>Do you have any sugesstions how we can improve?</p>-->
+        <!--<p>Do you want to cooperate with us?</p>-->
+        <!--<p>Contact us using email: <a href="mailto:contact.quickit@gmail.com">contact.quickit@gmail.com</a></p>-->
+        <div class="row justify-content-end">
+            <!--<div class="col-3"></div>-->
+            <!--<div class="col-3"></div>-->
+            <div class="col-sm-3">
+                <h4><strong>QuickIT Production</strong></h4>
+                <p>We are a small startup with huge ambitions. We deliver small projects based on our ideas but we are open for cooperation. Our main goal is to make ourselves better everyday.</p>
+            </div>
+            <div class="col-sm-3">
+                <h4><strong>Contact US</strong></h4>
+                <p>Have you found a bug?</p>
+                <p>Do you have any sugesstions how we can improve?</p>
+                <p>Do you want to cooperate with us?</p>
+                <p>Contact us by email: <a href="mailto:quickitproduction@gmail.com">contact.quickit@gmail.com</a></p>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+.footer {
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: justify;
+  background-color: #343A40;
+  color: #96999C;
+  font-style: italic;
+  margin-top: 0px;
+  border: none;
+  display: block;
+  margin: 0 auto -155px;
+}
+
+p {
+    white-space: pre-line;
+    margin: 0px;
+    padding: 0px;;
+}
+
+h4 {
+    margin-top: 8px;
+}
+</style>
