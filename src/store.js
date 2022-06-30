4f6a63ef8e6dc8f0cffb050ae5c050508772511e
lukasz kulyk <lukasz.kulyk@gmail.com>
2022-06-30 13:17:39 +0200

    11th game veek value change.

--- src/store.js
+++ src/store.js
@@ -35,7 +35,7 @@ export default new Vuex.Store({
         fullSchedule: [],
         lastGameWeekPlayed: [],
         lastGameWeekResults: [],
-		nextGameWeekToBeBet: 10,
+		nextGameWeekToBeBet: 11,
 		nextGameWeekGames: [],
 		allPlayedGamesResults: [],
 		currentGameWeekGamesAdminView: [],
