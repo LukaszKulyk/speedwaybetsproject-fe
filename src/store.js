8b66fde39fd2dcfb4edec814d479fd35995506ba
lukasz kulyk <lukasz.kulyk@gmail.com>
2022-07-08 09:25:35 +0200

    changed nextGameWeek value.

--- src/store.js
+++ src/store.js
@@ -35,7 +35,7 @@ export default new Vuex.Store({
         fullSchedule: [],
         lastGameWeekPlayed: [],
         lastGameWeekResults: [],
-		nextGameWeekToBeBet: 11,
+		nextGameWeekToBeBet: 12,
 		nextGameWeekGames: [],
 		allPlayedGamesResults: [],
 		currentGameWeekGamesAdminView: [],
